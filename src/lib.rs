2eef813b9f3909ebca541291ae04467c1c1157ed
Zombieliu <17600047539@163.com>
2021-08-10 19:53:21 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,112 +1,67 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::{LookupMap, UnorderedMap, UnorderedSet};
-use near_sdk::json_types::{ValidAccountId, U128, U64};
 use near_sdk::serde::{Deserialize, Serialize};
-use near_sdk::{
-    assert_one_yocto, env, ext_contract, near_bindgen, AccountId, Balance, Gas, PanicOnDefault,
-    Promise, PromiseOrValue, CryptoHash, BorshStorageKey,
-};
-use std::cmp::min;
-use std::collections::HashMap;
-
-use crate::external::*;
-use crate::internal::*;
-use crate::sale::*;
+use near_sdk::collections::{UnorderedSet, LookupMap};
+use near_sdk::{near_bindgen, AccountId, PanicOnDefault, BorshStorageKey, Balance, assert_one_yocto, Promise};
+use near_sdk::env;
 use near_sdk::env::STORAGE_PRICE_PER_BYTE;
+use near_contract_standards::storage_management::StorageBalanceBounds;
+use near_sdk::json_types::U128;
+use near_contract_standards::non_fungible_token::TokenId;
+use near_sdk::borsh::maybestd::collections::HashMap;
+use near_sdk::log;
 
-mod external;
-mod ft_callbacks;
+
 mod internal;
 mod nft_callbacks;
-mod sale;
-mod sale_views;
-mod test;
 
 
-// TODO check seller supports storage_deposit at ft_token_id they want to post sale in
-
-const GAS_FOR_FT_TRANSFER: Gas = 5_000_000_000_000;
-/// greedy max Tgas for resolve_purchase
-const GAS_FOR_ROYALTIES: Gas = 115_000_000_000_000;
-const GAS_FOR_NFT_TRANSFER: Gas = 15_000_000_000_000;
-const BID_HISTORY_LENGTH_DEFAULT: u8 = 1;
-const NO_DEPOSIT: Balance = 0;
+/// per byes
 const STORAGE_PER_SALE: u128 = 1000 * STORAGE_PRICE_PER_BYTE;
-static DELIMETER: &str = "||";
 
-
-pub type SaleConditions = HashMap<FungibleTokenId, U128>;
-pub type Bids = HashMap<FungibleTokenId, Vec<Bid>>;
-pub type TokenId = String;
-pub type TokenType = Option<String>;
 pub type FungibleTokenId = AccountId;
 pub type ContractAndTokenId = String;
-// TODO: Capital U128
-pub type Payout = HashMap<AccountId, U128>;
-
-// // TODO: Royalties set
+pub type SaleConditions = HashMap<FungibleTokenId, U128>;
 pub type Amount = u8;
-pub type Royalties = Option<HashMap<ContractAndTokenId,Amount>> ;
-
+pub type Royalties = Option<HashMap<ContractAndTokenId,Amount>>;
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
     pub owner_id: AccountId,
-    //predecessor_account_id + || + TokenID + Sale struct
-    pub sales: UnorderedMap<ContractAndTokenId, Sale>,
-    pub by_owner_id: LookupMap<AccountId, UnorderedSet<ContractAndTokenId>>,
-    pub by_nft_contract_id: LookupMap<AccountId, UnorderedSet<TokenId>>,
     pub ft_token_ids: UnorderedSet<AccountId>,
     pub storage_deposits: LookupMap<AccountId, Balance>,
-    pub bid_history_length: u8,
+    pub by_owner_id: LookupMap<AccountId, UnorderedSet<ContractAndTokenId>>,
 }
 
 /// Helper structure to for keys of the persistent collections.
 #[derive(BorshStorageKey, BorshSerialize)]
 pub enum StorageKey {
-    Sales,
-    ByOwnerId,
-    ByOwnerIdInner { account_id_hash: CryptoHash },
-    ByNFTContractId,
-    ByNFTContractIdInner { account_id_hash: CryptoHash },
     FTTokenIds,
     StorageDeposits,
+    ByOwnerId,
 }
 
 #[near_bindgen]
 impl Contract {
     #[init]
-    pub fn new(owner_id: ValidAccountId, ft_token_ids:Option<Vec<ValidAccountId>>, bid_history_length:Option<u8>) -> Self {
-        let mut this = Self {
-            owner_id: owner_id.into(),
-            sales: UnorderedMap::new(StorageKey::Sales),
-            by_owner_id: LookupMap::new(StorageKey::ByOwnerId),
-            by_nft_contract_id: LookupMap::new(StorageKey::ByNFTContractId),
+    pub fn new(owner_id: AccountId){
+       let mut this =  Self {
+            owner_id,
             ft_token_ids: UnorderedSet::new(StorageKey::FTTokenIds),
             storage_deposits: LookupMap::new(StorageKey::StorageDeposits),
-            bid_history_length: bid_history_length.unwrap_or(BID_HISTORY_LENGTH_DEFAULT),
-        };
+            by_owner_id: LookupMap::new(StorageKey::ByOwnerId),
+       };
         // support NEAR by default
-        this.ft_token_ids.insert(&"near".to_string());
-
-        // add all input FT tokens address
-        if let Some(ft_token_ids) = ft_token_ids {
-            for ft_token_id in ft_token_ids {
-                this.ft_token_ids.insert(ft_token_id.as_ref());
-            }
-        }
-        this
+       this.ft_token_ids.insert(&AccountId::new_unchecked("near".to_string()));
     }
 
-    /// only owner can use this function to add ft tokens
-    pub fn add_ft_token_ids(&mut self, ft_token_ids: Vec<ValidAccountId>) -> Vec<bool> {
-        // check owner
+    /// only contract owner can add support FT token list
+    /// and return success result
+    pub fn add_ft_token_ids(&mut self, ft_token_ids: Vec<AccountId>) -> Vec<bool> {
         self.assert_owner();
         let mut added = vec![];
-        // for to add all FT tokens address
         for ft_token_id in ft_token_ids {
-            added.push(self.ft_token_ids.insert(ft_token_id.as_ref()));
+            added.push(self.ft_token_ids.insert(&ft_token_id));
         }
         added
     }
@@ -114,11 +69,13 @@ impl Contract {
     /// TODO remove token (should check if sales can complete even if owner stops supporting token type)
 
     #[payable]
-    pub fn storage_deposit(&mut self, account_id: Option<ValidAccountId>) {
+    pub fn storage_deposit(&mut self, account_id: Option<AccountId>) {
         let storage_account_id = account_id
             .map(|a| a.into())
             .unwrap_or_else(env::predecessor_account_id);
+
         let deposit = env::attached_deposit();
+        // about 1kb 10TG
         assert!(
             deposit >= STORAGE_PER_SALE,
             "Requires minimum deposit of {}",
@@ -136,26 +93,44 @@ impl Contract {
         let mut amount = self.storage_deposits.remove(&owner_id).unwrap_or(0);
         let sales = self.by_owner_id.get(&owner_id);
         let len = sales.map(|s| s.len()).unwrap_or_default();
-        amount -= u128::from(len) * STORAGE_PER_SALE;
+        let diff = u128::from(len) * STORAGE_PER_SALE;
+        amount -= diff;
         if amount > 0 {
-            Promise::new(owner_id).transfer(amount);
+            Promise::new(owner_id.clone()).transfer(amount);
+        }
+        if diff > 0 {
+            self.storage_deposits.insert(&owner_id, &diff);
         }
     }
 
     /// views
 
-    //support token list
     pub fn supported_ft_token_ids(&self) -> Vec<AccountId> {
         self.ft_token_ids.to_vec()
     }
 
-    // return price of every sale
-    pub fn storage_amount(&self) -> U128 {
+    pub fn storage_balance_bounds(&self) -> StorageBalanceBounds {
+        StorageBalanceBounds {
+            min: U128(STORAGE_PER_SALE),
+            max: None,
+        }
+    }
+
+    pub fn storage_minimum_balance(&self) -> U128 {
         U128(STORAGE_PER_SALE)
     }
 
-    //return the account storage balance
-    pub fn storage_paid(&self, account_id: ValidAccountId) -> U128 {
-        U128(self.storage_deposits.get(account_id.as_ref()).unwrap_or(0))
+    pub fn storage_balance_of(&self, account_id: AccountId) -> U128 {
+        U128(self.storage_deposits.get(&account_id).unwrap_or(0))
+    }
+
+    /// deprecated
+
+    pub fn storage_paid(&self, account_id: AccountId) -> U128 {
+        U128(self.storage_deposits.get(&account_id).unwrap_or(0))
+    }
+
+    pub fn storage_amount(&self) -> U128 {
+        U128(STORAGE_PER_SALE)
     }
 }
--- tests/test.rs
+++ tests/test.rs
@@ -1,36 +1,61 @@
 use std::convert::TryInto;
 use near_contract_standards::non_fungible_token::metadata::TokenMetadata;
-use near_sdk::{AccountId, log};
-use near_sdk_sim::{ContractAccount, DEFAULT_GAS, STORAGE_AMOUNT, UserAccount, call, deploy, init_simulator, to_yocto, view};
-
-
+use near_sdk::{AccountId};
+use near_sdk_sim::{ContractAccount, STORAGE_AMOUNT, UserAccount, call, deploy, init_simulator, to_yocto, view};
 use non_fungible_token::ContractContract;
+use nft_marketpalces::ContractContract as MarketContract;
+
+
 
 near_sdk_sim::lazy_static_include::lazy_static_include_bytes! {
    pub TOKEN_WASM_BYTES => "res/non_fungible_token.wasm",
+   pub MARKET_PLACES_WASM_BYTES => "res/nft_marketplaces.wasm",
 }
 
-fn init(
+fn nft_init(
     initial_balance: u128,
 ) -> (UserAccount, ContractAccount<ContractContract>, UserAccount) {
     let mut genesis = near_sdk_sim::runtime::GenesisConfig::default();
     genesis.gas_price = 0;
     genesis.gas_limit = u64::MAX;
-    let master_account = init_simulator(Some(genesis));
-    let alice =
-        master_account.create_user(AccountId::new_unchecked("alice".to_string()), initial_balance);
+    let nft_master_account = init_simulator(Some(genesis));
+    let nft_alice =
+        nft_master_account.create_user(AccountId::new_unchecked("alice".to_string()), initial_balance);
 
-    let alice_account_id: AccountId = alice.account_id().try_into().unwrap();
+    let nft_alice_account_id: AccountId = nft_alice.account_id().try_into().unwrap();
 
-    let contract_account = deploy! {
+    let nft_contract_account = deploy! {
         contract: ContractContract,
-        contract_id: "contract",
+        contract_id: "contract1",
         bytes: &TOKEN_WASM_BYTES,
-        signer_account: master_account,
-        init_method: new_default_meta(alice_account_id)
+        signer_account: nft_alice,
+        init_method: new_default_meta(nft_alice_account_id)
     };
 
-    (master_account, contract_account, alice)
+    (nft_master_account, nft_contract_account, nft_alice)
+}
+
+fn market_places_init(
+    initial_balance: u128,
+) -> (UserAccount, ContractAccount<MarketContract>, UserAccount) {
+    let mut genesis = near_sdk_sim::runtime::GenesisConfig::default();
+    genesis.gas_price = 0;
+    genesis.gas_limit = u64::MAX;
+    let market_master_account = init_simulator(Some(genesis));
+    let market_bob =
+        market_master_account.create_user(AccountId::new_unchecked("bob".to_string()), initial_balance);
+
+    let bob_account_id: AccountId = market_bob.account_id().try_into().unwrap();
+
+    let market_contract_account = deploy! {
+        contract: MarketContract,
+        contract_id: "contract2",
+        bytes: &MARKET_PLACES_WASM_BYTES,
+        signer_account: market_bob,
+        init_method: new(bob_account_id)
+    };
+
+    (market_master_account, market_contract_account, market_bob)
 }
 
 fn sample_token_metadata() -> TokenMetadata {
@@ -52,24 +77,53 @@ fn sample_token_metadata() -> TokenMetadata {
 
 #[test]
 fn init_test() {
-    let (_master_account, _contract_account, _alice) = init(to_yocto("10000"));
+    let (_nft_master_account, _nft_contract_account, _nft_alice) = nft_init(to_yocto("10000"));
 }
 
 #[test]
 fn check_promise() {
-    let (_master_account, contract, alice) = init(to_yocto("10000"));
+    let (_nft_master_account, nft_contract, nft_alice) = nft_init(to_yocto("10000"));
+    let (market_master_account, market_contract_account, market_bob) = market_places_init(to_yocto("10000"));
+
+
+
     let token_id = "P001".to_string();
-    let token_owner_id = alice.account_id().try_into().unwrap();
+    //alice.near
+    let token_owner_id = nft_alice.account_id().try_into().unwrap();
+    // get metadata
     let token_metadata = sample_token_metadata();
-    let res =call!(
-        alice,
-        contract.nft_mint(token_id, token_owner_id,token_metadata),
+    // mint
+    let res1 =call!(
+        nft_alice,
+        nft_contract.nft_mint(token_id, token_owner_id,token_metadata),
         deposit = STORAGE_AMOUNT
     );
-    let promise_outcomes = res.get_receipt_results();
-    println!("{:#?}\n{:#?}", promise_outcomes, res);
-    let res2 = view!(contract.nft_token("P001".to_string()));
-    println!("{:#?}",res2.unwrap_json_value());
+    // let promise_outcomes = res1.get_receipt_results();
+    println!("{:#?}", res1);
 
-    // log!("{:#?}", res2);
+    let depositAccount = market_bob.account_id().try_into().unwrap();
+    // storage_deposit
+    let res2 =call!(
+        market_bob,
+        market_contract_account.storage_deposit(depositAccount),
+        deposit = STORAGE_AMOUNT
+    );
+    println!("{:#?}", res2);
+
+
+    // let res2 = view!(market_contract_account.supported_ft_token_ids());
+    // println!("{:#?}",res2.unwrap_json_value());
+
+    //contract2.near
+    // let account_id = market_contract_account.account_id();
+    // // println!("{:#?}", account_id);
+    // let msg = Some("arr".to_string());
+    // let token_id2 = "P001".to_string();
+    // let res2 =call!(
+    //     nft_alice,
+    //     nft_contract.nft_approve(token_id2, account_id,msg),
+    //     deposit = STORAGE_AMOUNT
+    // );
+    // println!("{:#?}", res2);
+
 }
